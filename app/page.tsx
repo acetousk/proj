0c0d1a1e1fd9505487361a0b1c05cc6d82a45f5d
acetousk <acetousk@users.noreply.github.com>
2024-09-26 23:33:41 -0600

    Just wow. Filters are crazy

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,25 @@ import { TypographyDemo } from "@/components/TypographyDemo";
 
 export default function Home() {
   return (
+      // https://medium.com/@erikritter/css-snippets-add-a-texture-overlay-to-an-entire-webpage-b0bfdfd02c45
       <div>
-          <ModeToggle/>
-          <TypographyDemo/>
-
-          <div className="paperOverlay"></div>
+          <div className="absolute inset-0 z-0">
+              <div id="parchment">
+              <ModeToggle/>
+              <TypographyDemo/>
+              <svg>
+                  <filter id="wavy2">
+                      <feTurbulence x="0" y="0" baseFrequency="0.02" numOctaves="5" seed="1"></feTurbulence>
+                      <feDisplacementMap in="SourceGraphic" scale="20"/>
+                  </filter>
+              </svg>
+              </div>
+          </div>
       </div>
-
+      //   <div className="absolute inset-0 bg-blue-200 z-10">Background Layer
+      //
+      //     <div className="absolute inset-0 bg-green-200 z-20 p-8 m-8">Middle Layer</div>
+      //     <div className="absolute inset-0 bg-red-200 z-30 p-8 m-12">
+    // </div>
   );
 }
