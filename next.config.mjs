6f94665196662357d5e5f33c04d9c926870a4217
acetousk <acetousk@users.noreply.github.com>
2024-10-02 00:21:40 -0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-        // Ensure valid configurations for image optimizations if needed
-        unoptimized: true,  // You might need this for static exports
-    }
+    basePath: "/proj",
 };
 
 export default nextConfig;
