5841a433b6e5867d7e812fea6bcf0aa905117e98
acetousk <acetousk@users.noreply.github.com>
2024-10-02 02:00:37 -0600

    Get rive working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,24 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-export const metadata: Metadata = {
-  title: "Proj",
-  description: "Do cool stuff",
+export const metadata = {
+  title: "Explore Page",
+  description: "Rive hero demo",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <link
+        rel="preload"
+        href="/hero_use_case.riv"
+        as="fetch"
+        crossOrigin="anonymous"
+      />
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,10 @@
-// import dynamic from 'next/dynamic';
-
-// Dynamically import RiveVehiclesDemo with SSR disabled
-// const RiveVehiclesDemo = dynamic(() => import('@/components/RiveVehiclesDemo'), { ssr: false });
-
-import {Button} from "@/components/ui/button";
-import Link from "next/link";
+import RiveHero from "@/components/RiveHero";
+// import RiveButton from "@/components/RiveButton";
 
 export default function Home() {
   return (
-      // <div>
-      //     <RiveVehiclesDemo/>
-      // </div>
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-        <div className="flex flex-col w-full justify-center items-center">
-            <Button>
-                <Link href="/v0">v0</Link>
-            </Button>
-        </div>
-
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
-    </div>
+    <main className="block relative w-screen h-screen">
+      <RiveHero />
+    </main>
   );
 }
