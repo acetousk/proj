ec20f385d845a954cbadc371aa37914d912c5f73
acetousk <acetousk@users.noreply.github.com>
2024-10-02 03:49:27 -0600

    Got rive and tailwind/shadcn workinggit status

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
-import RiveHero from "@/components/RiveHero";
-// import RiveButton from "@/components/RiveButton";
+// import dynamic from 'next/dynamic';
+
+// Dynamically import RiveVehiclesDemo with SSR disabled
+// const RiveVehiclesDemo = dynamic(() => import('@/components/RiveVehiclesDemo'), { ssr: false });
+
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="block relative w-screen h-screen">
-      <RiveHero />
-    </main>
-  );
+    return (
+        // <div>
+        //     <RiveVehiclesDemo/>
+        // </div>
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+                <div className="flex flex-col w-full justify-center items-center">
+                    <Button>
+                        <Link href="/v0">v0</Link>
+                    </Button>
+                </div>
+
+            </main>
+            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+            </footer>
+        </div>
+    );
 }
