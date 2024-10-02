1b833fa0998e63e97482b6f51de1d8b81ed1391c
acetousk <acetousk@users.noreply.github.com>
2024-10-02 02:17:34 -0600

    Try to update mjs and packages, but got weird errors

--- next.config.mjs
+++ next.config.mjs
@@ -40,8 +40,10 @@ const nextConfig = {
   },
 };
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer(nextConfig);
+export default bundleAnalyzer(nextConfig);
