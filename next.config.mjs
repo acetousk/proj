dd89bd0fa44d002fb0cb6c7810878057b078a1d8
acetousk <acetousk@users.noreply.github.com>
2024-10-01 23:58:55 -0600

    fix?

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: {
+        // Ensure valid configurations for image optimizations if needed
+        unoptimized: true,  // You might need this for static exports
+    }
 };
 
 export default nextConfig;
